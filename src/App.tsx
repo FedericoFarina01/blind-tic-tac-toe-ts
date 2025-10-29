24b55d0904255b408492a5158399d1dca2badcdb
Nicolas Accetta <nicolas.accetta@gmail.com>
2025-10-29 11:51:23 -0300

    feat: add board

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import "./App.css";
+
+const WINNING_LINES = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+type Square = null | "X" | "O";
+function App() {
+  const [squares, setSquares] = useState<Square[]>(Array(9).fill(null));
+  return (
+    <main>
+      <section className="board">
+        {squares.map((square, index) => {
+          const id = `square-${index}`;
+          return (
+            <button className="cell" key={id} type="button">
+              {square}
+            </button>
+          );
+        })}
+      </section>
+    </main>
+  );
+}
+
+export default App;
